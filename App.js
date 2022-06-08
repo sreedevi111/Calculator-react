3ce3747f13765daad2d55c4a03ce6bfdf584cd2b
sreedevi111 <sreedevisobhana@gmail.com>
2022-06-08 21:41:36 +0530

    implemented calculator

--- App.js
+++ App.js
@@ -1,20 +1,31 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import { SafeAreaView, StyleSheet, Switch, Text } from 'react-native';
+import { myColors } from './src/styles/Colors';
+import { ThemeContext } from './src/context/ThemeContext';
+import MyKeyboard from './src/components/MyKeyboard';
+
+
 
 export default function App() {
+  const [theme, setTheme] = useState('light');
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <ThemeContext.Provider value={theme}>
+      <SafeAreaView style={theme === 'light' ? styles.container : [styles.container, {backgroundColor: 'black'}]}>
+        <Switch
+          value={theme === 'dark'} //if theme dark, value of switch is true
+          onValueChange={() => setTheme(theme === 'light' ? 'dark' : 'light')}
+        />
+        <MyKeyboard />
+      </SafeAreaView>
+    </ThemeContext.Provider>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: myColors.light,
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'flex-start',
   },
 });
---
+++ src/context/ThemeContext.js
@@ -0,0 +1,4 @@
+import { createContext } from 'react';
+//context provide way to pass the data through component tree, without actually passing it in each level
+
+export const ThemeContext = createContext('light');
